3b9687b00a742d137b74c9ecfd5021c867cfae5a
NezFeeLitNoeud <nepheline.nehlig@gmail.com>
2019-05-31 12:13:23 +0200

    take off the js function that is making the link work

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 
-let moreExperience = document.getElementById("more"); // Lien a
+// let moreExperience = document.getElementById("more"); // Lien a
 
-moreExperience.addEventListener("click", function(){
-document.querySelectorAll(".cacher").forEach(div => {
-	if (div.style.display === "block") {
-		div.style.display = "none";
-		document.getElementById("grid").style.height = "300px";
-		moreExperience.innerHTML = "Plus de resultat pour \"Experience\"";
+// moreExperience.addEventListener("click", function(){
+// document.querySelectorAll(".cacher").forEach(div => {
+// 	if (div.style.display === "block") {
+// 		div.style.display = "none";
+// 		document.getElementById("grid").style.height = "300px";
+// 		moreExperience.innerHTML = "Plus de resultat pour \"Experience\"";
 
-	} else {
-		div.style.display = "block";
-		document.getElementById("grid").style.height = "605px";
-		moreExperience.innerHTML = "Moins de resultat pour \"Experience\"";
-	}
+// 	} else {
+// 		div.style.display = "block";
+// 		document.getElementById("grid").style.height = "605px";
+// 		moreExperience.innerHTML = "Moins de resultat pour \"Experience\"";
+// 	}
 	
-})
-});
+// })
+// });
 
 // let moreExperience = document.getElementById("more"); // Lien a
 
