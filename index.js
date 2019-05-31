f790114030ee74e0697ef2e0536d13930aa0d477
NezFeeLitNoeud <nepheline.nehlig@gmail.com>
2019-05-31 12:06:54 +0200

    add a link to show more experience made in js

---
+++ index.js
@@ -0,0 +1,30 @@
+
+let moreExperience = document.getElementById("more"); // Lien a
+
+moreExperience.addEventListener("click", function(){
+document.querySelectorAll(".cacher").forEach(div => {
+	if (div.style.display === "block") {
+		div.style.display = "none";
+		document.getElementById("grid").style.height = "300px";
+		moreExperience.innerHTML = "Plus de resultat pour \"Experience\"";
+
+	} else {
+		div.style.display = "block";
+		document.getElementById("grid").style.height = "605px";
+		moreExperience.innerHTML = "Moins de resultat pour \"Experience\"";
+
+	}
+	
+})
+});
+
+// let moreExperience = document.getElementById("more"); // Lien a
+
+// moreExperience.addEventListener("click", function(){
+// document.getElementById("grid").style.height = "600px";
+// document.querySelectorAll(".cacher").forEach(div => {
+// 	div.style.display = "block";
+// })
+// });
+
+
