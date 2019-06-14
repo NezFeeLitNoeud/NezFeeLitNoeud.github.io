6762b66c3d607a1583faf9880ef52939ddf0b7bb
NezFeeLitNoeud <nepheline.nehlig@gmail.com>
2019-06-14 17:26:32 +0200

    made the code more readable and add some comments for JS

--- index.js
+++ index.js
@@ -1,71 +1,47 @@
 let moreExperience = document.getElementById("more"); // Lien a
 
-// moreExperience.addEventListener("click", function(){
-// document.querySelectorAll(".cacher").forEach(div => {
-// 	if (div.style.display === "block") {
-// 		div.style.display = "none";
-// 		document.getElementById("grid").style.height = "300px";
-// 		moreExperience.innerHTML = "Plus de resultat pour \"Experience\"";
-// 		document.getElementById("footer").style.marginTop = "400px";
-
-// 	} else {
-// 		div.style.display = "block";
-// 		document.getElementById("grid").style.height = "605px";
-// 		moreExperience.innerHTML = "Moins de resultat pour \"Experience\"";
-// 		document.getElementById("footer").style.marginTop = "710px";
-// 	}
-	
-// })
-// });
-
-// let moreExperience = document.getElementById("more"); // Lien a
-
-// moreExperience.addEventListener("click", function(){
-// document.getElementById("grid").style.height = "600px";
-// document.querySelectorAll(".cacher").forEach(div => {
-// 	div.style.display = "block";
-// })
-// });
-
-let queries = false;
+// Add an event Listener on click on the more element
 moreExperience.addEventListener("click", function(){
 document.querySelectorAll(".cacher").forEach(div => {
 
-
 function myFunction(x) {
-  if (x.matches) { // If media query matches
+  // If media query matches,
+  if (x.matches) { 
+  		// if the div style display matched block, then we make all the changes on the div
 		if (div.style.display === "block") {
-			console.log("blue");
 			div.style.display = "none";
 			document.getElementById("grid").style.height = "300px";
-			moreExperience.innerHTML = "Plus de resultat pour \"Experiences\"";
+			moreExperience.innerHTML = "Plus de resultat pour \"Expériences\"";
 			document.getElementById("footer").style.marginTop = "430px";
 
+		// Otherwise, we make the contrary
 		} else {
-			console.log("purple");
 			div.style.display = "block";
 			document.getElementById("grid").style.height = "605px";
-			moreExperience.innerHTML = "Moins de resultat pour \"Experiences\"";
+			moreExperience.innerHTML = "Moins de resultat pour \"Expériences\"";
 			document.getElementById("footer").style.marginTop = "730px";
 		}
+	// if media queries doesn't match, 
   } else {
+  	// if the div style display matched block, then we make all the changes on the div
   	if (div.style.display === "block") {
 			console.log("red");
 			div.style.display = "none";
 			document.getElementById("grid").style.height = "300px";
-			moreExperience.innerHTML = "Plus de resultat pour \"Experiences\"";
+			moreExperience.innerHTML = "Plus de resultat pour \"Expériences\"";
 			document.getElementById("footer").style.marginTop = "400px";
 
 		} else {
 			console.log("orange");
 			div.style.display = "block";
 			document.getElementById("grid").style.height = "605px";
-			moreExperience.innerHTML = "Moins de resultat pour \"Experiences\"";
+			moreExperience.innerHTML = "Moins de resultat pour \"Expériences\"";
 			document.getElementById("footer").style.marginTop = "710px";
 		}
   	}
 }
 
+//  create a var x with the method matchMedia that features the width of the screen
 var x = window.matchMedia("(max-width: 1130px)")
 myFunction(x) // Call listener function at run time
 x.addListener(myFunction) // Attach listener function on state changes
