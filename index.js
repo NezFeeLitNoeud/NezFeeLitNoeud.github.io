dc569fda7c42ec8e657a5dea2bdea0227be659a3
NezFeeLitNoeud <nepheline.nehlig@gmail.com>
2019-06-14 15:23:14 +0200

    add a function to play with the media queries and the class more that display exp

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 let moreExperience = document.getElementById("more"); // Lien a
 
-moreExperience.addEventListener("click", function(){
-document.querySelectorAll(".cacher").forEach(div => {
-	if (div.style.display === "block") {
-		div.style.display = "none";
-		document.getElementById("grid").style.height = "300px";
-		moreExperience.innerHTML = "Plus de resultat pour \"Experience\"";
-		document.getElementById("footer").style.marginTop = "400px";
+// moreExperience.addEventListener("click", function(){
+// document.querySelectorAll(".cacher").forEach(div => {
+// 	if (div.style.display === "block") {
+// 		div.style.display = "none";
+// 		document.getElementById("grid").style.height = "300px";
+// 		moreExperience.innerHTML = "Plus de resultat pour \"Experience\"";
+// 		document.getElementById("footer").style.marginTop = "400px";
 
-	} else {
-		div.style.display = "block";
-		document.getElementById("grid").style.height = "605px";
-		moreExperience.innerHTML = "Moins de resultat pour \"Experience\"";
-		document.getElementById("footer").style.marginTop = "710px";
-	}
+// 	} else {
+// 		div.style.display = "block";
+// 		document.getElementById("grid").style.height = "605px";
+// 		moreExperience.innerHTML = "Moins de resultat pour \"Experience\"";
+// 		document.getElementById("footer").style.marginTop = "710px";
+// 	}
 	
-})
-});
+// })
+// });
 
 // let moreExperience = document.getElementById("more"); // Lien a
 
@@ -27,4 +27,48 @@ document.querySelectorAll(".cacher").forEach(div => {
 // })
 // });
 
+let queries = false;
+moreExperience.addEventListener("click", function(){
+document.querySelectorAll(".cacher").forEach(div => {
 
+
+function myFunction(x) {
+  if (x.matches) { // If media query matches
+		if (div.style.display === "block") {
+			console.log("blue");
+			div.style.display = "none";
+			document.getElementById("grid").style.height = "300px";
+			moreExperience.innerHTML = "Plus de resultat pour \"Experiences\"";
+			document.getElementById("footer").style.marginTop = "430px";
+
+		} else {
+			console.log("purple");
+			div.style.display = "block";
+			document.getElementById("grid").style.height = "605px";
+			moreExperience.innerHTML = "Moins de resultat pour \"Experiences\"";
+			document.getElementById("footer").style.marginTop = "730px";
+		}
+  } else {
+  	if (div.style.display === "block") {
+			console.log("red");
+			div.style.display = "none";
+			document.getElementById("grid").style.height = "300px";
+			moreExperience.innerHTML = "Plus de resultat pour \"Experiences\"";
+			document.getElementById("footer").style.marginTop = "400px";
+
+		} else {
+			console.log("orange");
+			div.style.display = "block";
+			document.getElementById("grid").style.height = "605px";
+			moreExperience.innerHTML = "Moins de resultat pour \"Experiences\"";
+			document.getElementById("footer").style.marginTop = "710px";
+		}
+  	}
+}
+
+var x = window.matchMedia("(max-width: 1130px)")
+myFunction(x) // Call listener function at run time
+x.addListener(myFunction) // Attach listener function on state changes
+
+})
+});
